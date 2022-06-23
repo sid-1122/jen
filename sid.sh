99d2f250e403bbd3ef86028d05fbdc96a324dfe3
sid-1122 <68847493+sid-1122@users.noreply.github.com>
2022-06-23 11:19:16 +0530

    Update sid.sh

--- sid.sh
+++ sid.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+START_DATE=`date -d "${days_backwards:-20} day ago" +%F `
 
+printf "%s\n" "Listing AMIs for region useast since ${START_DATE}" 
+aws ec2 describe-images --region us-east-1 --output table \
+--filters Name=is-public,Values=false \
+--query "sort_by(Images[?CreationDate>=\`${START_DATE}\`],&CreationDate)[].[Name,ImageId,ImageType,CreationDate,State]"
